3e4f2300744e29746c32d95d376f8b29bb9d5f63
yeqianfeng911@gmail.com <yeqianfeng@NicoledeiMac.local>
2019-12-22 23:45:42 +0800

    add future weather display
    Signed-off-by: yeqianfeng911@gmail.com <yeqianfeng@NicoledeiMac.local>

--- app/src/main/java/com/xhliyuxiao/weather/MainActivity.java
+++ app/src/main/java/com/xhliyuxiao/weather/MainActivity.java
@@ -3,7 +3,6 @@ package com.xhliyuxiao.weather;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -25,6 +24,7 @@ import com.xhliyuxiao.weather.utils.NetUtil;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -39,9 +39,29 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv, temperatureTv, weatherTv, windTv, cityNameTv;
 
+
+    // 第1天
+    private TextView week1T, temp1T, weather1T, wind1T;
+
+    // 第2天
+    private TextView week2T, temp2T, weather2T, wind2T;
+
+    // 第3天
+    private TextView week3T, temp3T, weather3T, wind3T;
+
+    // 第4天
+    private TextView week4T, temp4T, weather4T, wind4T;
+
+    // 第5天
+    private TextView week5T, temp5T, weather5T, wind5T;
+
+    // 第6天
+    private TextView week6T, temp6T, weather6T, wind6T;
+
+
     private ImageView weatherImg, pmImg;
 
-    private WeatherInfo weatherToday;
+    private List<WeatherInfo> weatherFuture;
 
     @SuppressLint("HandlerLeak")
     private Handler handler = new Handler() {
@@ -49,7 +69,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         public void handleMessage(@NonNull Message msg) {
             switch (msg.what) {
                 case UPDATE_TODAY_WEATHER:
-                    updateTodayWeatherView();
+                    updateWeatherVIew();
                     break;
                 default:
                     break;
@@ -73,6 +93,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     private void initView() {
+        // today
         updateViewBtn = findViewById(R.id.title_update_btn);
         updateViewBtn.setOnClickListener(this);
 
@@ -102,6 +123,67 @@ public class MainActivity extends Activity implements View.OnClickListener {
         temperatureTv.setText("N/A");
         weatherTv.setText("N/A");
         windTv.setText("N/A");
+
+        // 第1天
+        week1T = findViewById(R.id.week1T);
+        temp1T = findViewById(R.id.temp1T);
+        weather1T = findViewById(R.id.weather1T);
+        wind1T = findViewById(R.id.wind1T);
+        week1T.setText("N/A");
+        temp1T.setText("N/A");
+        weather1T.setText("N/A");
+        wind1T.setText("N/A");
+
+        // 第2天
+        week2T = findViewById(R.id.week2T);
+        temp2T = findViewById(R.id.temp2T);
+        weather2T = findViewById(R.id.weather2T);
+        wind2T = findViewById(R.id.wind2T);
+        week2T.setText("N/A");
+        temp2T.setText("N/A");
+        weather2T.setText("N/A");
+        wind2T.setText("N/A");
+
+        // 第3天
+        week3T = findViewById(R.id.week3T);
+        temp3T = findViewById(R.id.temp3T);
+        weather3T = findViewById(R.id.weather3T);
+        wind3T = findViewById(R.id.wind3T);
+        week3T.setText("N/A");
+        temp3T.setText("N/A");
+        weather3T.setText("N/A");
+        wind3T.setText("N/A");
+
+        // 第4天
+        week4T = findViewById(R.id.week4T);
+        temp4T = findViewById(R.id.temp4T);
+        weather4T = findViewById(R.id.weather4T);
+        wind4T = findViewById(R.id.wind4T);
+        week4T.setText("N/A");
+        temp4T.setText("N/A");
+        weather4T.setText("N/A");
+        wind4T.setText("N/A");
+
+        // 第5天
+        week5T = findViewById(R.id.week5T);
+        temp5T = findViewById(R.id.temp5T);
+        weather5T = findViewById(R.id.weather5T);
+        wind5T = findViewById(R.id.wind5T);
+        week5T.setText("N/A");
+        temp5T.setText("N/A");
+        weather5T.setText("N/A");
+        wind5T.setText("N/A");
+
+        // 第6天
+        week6T = findViewById(R.id.week6T);
+        temp6T = findViewById(R.id.temp6T);
+        weather6T = findViewById(R.id.weather6T);
+        wind6T = findViewById(R.id.wind6T);
+        week6T.setText("N/A");
+        temp6T.setText("N/A");
+        weather6T.setText("N/A");
+        wind6T.setText("N/A");
+
     }
 
     @Override
@@ -118,43 +200,93 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private void startSelectCityActivity() {
         Intent intent = new Intent(this, SelectCityActivity.class);
-        intent.putExtra("cityName", this.weatherToday.getCitynm());
+        intent.putExtra("cityName", this.weatherFuture.get(0).getCitynm());
         startActivity(intent);
         overridePendingTransition(0, 0);
         finish();
     }
 
+    private void updateWeatherVIew() {
+        updateTodayWeatherView();
+        updateT1();
+        updateT2();
+        updateT3();
+        updateT4();
+        updateT5();
+        updateT6();
+    }
+
     @SuppressLint("SetTextI18n")
     private void updateTodayWeatherView() {
-        cityNameTv.setText(this.weatherToday.getCitynm() + "天气");
-        cityTv.setText(this.weatherToday.getCitynm());
-        timeTv.setText(this.weatherToday.getDays());
-        humidityTv.setText("湿度 " + this.weatherToday.getHumidity());
-//        pmQualityTv.setText(this.weatherToday.getAqi());
-        pmDataTv.setText(this.weatherToday.getAqi());
-        weekTv.setText(this.weatherToday.getWeek());
-        temperatureTv.setText(this.weatherToday.getTemperature().replace("/", "~"));
-        weatherTv.setText(this.weatherToday.getWeather());
-        windTv.setText(this.weatherToday.getWind());
+        cityNameTv.setText(this.weatherFuture.get(0).getCitynm() + "天气");
+        cityTv.setText(this.weatherFuture.get(0).getCitynm());
+        timeTv.setText(this.weatherFuture.get(0).getDays());
+        humidityTv.setText("湿度 " + this.weatherFuture.get(0).getHumidity());
+        pmDataTv.setText(this.weatherFuture.get(0).getAqi());
+        weekTv.setText(this.weatherFuture.get(0).getWeek());
+        temperatureTv.setText(this.weatherFuture.get(0).getTemperature().replace("/", "~"));
+        weatherTv.setText(this.weatherFuture.get(0).getWeather());
+        windTv.setText(this.weatherFuture.get(0).getWind());
     }
+
+    private void updateT1() {
+        week1T.setText(this.weatherFuture.get(1).getWeek());
+        temp1T.setText(this.weatherFuture.get(1).getTemperature());
+        weather1T.setText(this.weatherFuture.get(1).getWeather());
+        wind1T.setText(this.weatherFuture.get(1).getWind());
+    }
+
+    private void updateT2() {
+        week2T.setText(this.weatherFuture.get(2).getWeek());
+        temp2T.setText(this.weatherFuture.get(2).getTemperature());
+        weather2T.setText(this.weatherFuture.get(2).getWeather());
+        wind2T.setText(this.weatherFuture.get(2).getWind());
+    }
+
+    private void updateT3() {
+        week3T.setText(this.weatherFuture.get(3).getWeek());
+        temp3T.setText(this.weatherFuture.get(3).getTemperature());
+        weather3T.setText(this.weatherFuture.get(3).getWeather());
+        wind3T.setText(this.weatherFuture.get(3).getWind());
+    }
+
+    private void updateT4() {
+        week4T.setText(this.weatherFuture.get(4).getWeek());
+        temp4T.setText(this.weatherFuture.get(4).getTemperature());
+        weather4T.setText(this.weatherFuture.get(4).getWeather());
+        wind4T.setText(this.weatherFuture.get(4).getWind());
+    }
+
+    private void updateT5() {
+        week5T.setText(this.weatherFuture.get(5).getWeek());
+        temp5T.setText(this.weatherFuture.get(5).getTemperature());
+        weather5T.setText(this.weatherFuture.get(5).getWeather());
+        wind5T.setText(this.weatherFuture.get(5).getWind());
+    }
+
+    private void updateT6() {
+        week6T.setText(this.weatherFuture.get(6).getWeek());
+        temp6T.setText(this.weatherFuture.get(6).getTemperature());
+        weather6T.setText(this.weatherFuture.get(6).getWeather());
+        wind6T.setText(this.weatherFuture.get(6).getWind());
+    }
+
 
     private void updateWeather() {
 
         if (NetUtil.getNetworkState(this) != NetUtil.NETWORK_NONE) {
             Log.d("myWeather", "网络OK");
             Toast.makeText(MainActivity.this, "网络OK！", Toast.LENGTH_LONG).show();
-//            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
-//            String cityCode = sharedPreferences.getString("main_city_code", "杭州");
             Log.d("Weather", currentCityName);
-            getTodayWeather(currentCityName);
+            getFutureWeather(currentCityName);
         } else {
             Log.d("myWeather", "网络挂了");
             Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void getTodayWeather(String city) {
-        final String url = Constant.WEATHER_TODAY + city;
+    private void getFutureWeather(String city) {
+        final String url = Constant.WEATHER_FUTURE + city;
         Log.d(TAG, "getWeather: " + url);
 
         new Thread(new Runnable() {
@@ -163,7 +295,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 try {
                     String response = HttpClient.get(url);
                     Log.d(TAG, "run: " + response);
-                    parseWeatherToday(response);
+                    parseWeatherFuture(response);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -171,11 +303,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }).start();
     }
 
-    private void parseWeatherToday(String jsonStr) {
-        Type jsonType = new TypeToken<WeatherResponse<WeatherInfo>>() {
+    private void parseWeatherFuture(String jsonStr) {
+        Type jsonType = new TypeToken<WeatherResponse<List<WeatherInfo>>>() {
         }.getType();
-        WeatherResponse<WeatherInfo> weatherResponse = JsonUtil.parseJson(jsonStr, jsonType);
-        this.weatherToday = Objects.requireNonNull(weatherResponse).getResult();
+        WeatherResponse<List<WeatherInfo>> weatherResponse = JsonUtil.parseJson(jsonStr, jsonType);
+        this.weatherFuture = Objects.requireNonNull(weatherResponse).getResult();
 
         Message msg = new Message();
         msg.what = UPDATE_TODAY_WEATHER;
